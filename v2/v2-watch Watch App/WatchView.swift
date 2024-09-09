4296e5965b20c59cf51f1454d19c4ec5a4e2abe6
Alice Mao <amao@wustl.edu>
2024-09-09 14:18:00 -0500

    fixed timer control on watch

--- v2/v2-watch Watch App/WatchView.swift
+++ v2/v2-watch Watch App/WatchView.swift
@@ -1,79 +1,24 @@
 //
-//  Watch.swift
-//  v2-watch Watch App
+//  Watch_Landing_page.swift
+//  sail-v1-watch Watch App
 //
-//  Created by Alice Mao on 3/1/24.
+//  Created by Alice Mao on 12/8/23.
 //
 
 import Foundation
 import SwiftUI
+import HealthKit
+import WatchKit
 
-struct WatchView: View {
+struct Watch_Landing_View : View {
     @ObservedObject var connector = ConnectToWatch.connect
-    @ObservedObject var ExtendedSess = ExtendedSession()
+    @ObservedObject var timerObj = TimerControls.time
+    @State private var authorize: Bool = false
     
-    @State var timer: Timer?
-    @State var animationState: AnimationState
+    @State var animationState: AnimationState = AnimationState.at
     
     @State private var animationAmount = 1.0;
     @State private var animationDuration = 1.0;
-    
-    func toggleEnd(){
-        print("toggle end \(connector.playing)")
-        connector.playing = !connector.playing
-    }
-    
-    private func startSession(){
-        if ExtendedSess.session.state != WKExtendedRuntimeSessionState.running{
-            print("start session")
-            ExtendedSess.startExtendedSession()
-        }
-    }
-    
-    private func endSession(){
-        if ExtendedSess.session.state == WKExtendedRuntimeSessionState.running{
-            print("end session")
-            ExtendedSess.stopExtendedSession()
-        }
-    }
-    
-    func startTimer() {
-        var index = 0
-        
-        animationState = connector.pattern.animationState
-        
-        timer = .scheduledTimer(withTimeInterval: connector.pattern.duration, repeats: true) { timer in
-            if !connector.playing{
-                timer.invalidate()
-                print("end timer")
-                return
-            }
-            
-            let currHaptic = connector.pattern.HapticArray[index % connector.pattern.HapticArray.count]
-//            print("currHaptic: \(connector.pattern.name)")
-//            print("duration: \(connector.pattern.duration)")
-            Haptics.play(currHaptic: currHaptic)
-            index += 1
-        }
-    }
-    
-    func updateTimer() {
-        DispatchQueue.main.async {
-            print("updating")
-            connector.received = false
-            timer?.invalidate()
-            connector.playing = true
-            startSession()
-            startTimer()
-        }
-    }
-    
-    func resetTimer(){
-            DispatchQueue.main.async {
-            print("resetTimer receive")
-            timer?.invalidate()
-        }
-    }
     
     private func update(){
         print("updating timer")
@@ -83,55 +28,56 @@ struct WatchView: View {
         }
     }
     
+ 
+    private func printVal(){
+        print("reciev: \(connector.receivedInitial)")
+    }
+    
     var body: some View {
-        NavigationStack{
-            ScrollView{
-                VStack (alignment: .leading, spacing: 20){
-                    if !runProgram{
-                        Text("awaiting info from phone")
-                        let _ = self.resetTimer()
-                    } else if connector.pattern.name == "END"{
-                        Text("awaiting info from phone")
-                        let _ = self.resetTimer()
-                    }
-                    else{
-                        IndicatorView(animationState: animationState)
-                        let _ = self.startSession()
-                        Button(action:{
-                            print("end")
-                            let _ = self.resetTimer()
-                            toggleEnd()
-                            endSession()
-                            runProgram = false
-                        }){
-                            Text("End")
-                        }
-                    }
-                 
-                    //Buttons for user to try patterns before test begins
-                    if(connector.patternPackageReceived){
-                        if connector.updating{
-                            let _ = self.update()
-                            Text("updating...")
-                        }
-                        
-                        NavigationLink(destination: CarouselView(underPattern: connector.patternPackage.underPattern, atPattern: connector.patternPackage.atPattern, abovePattern: connector.patternPackage.abovePattern)) {
-                            Text("Test Patterns")
+        NavigationView{
+            VStack{
+                if !authorize{
+                    Text("need to authorize healthkit")
+                }
+                else if !connector.receivedInitial{
+                    Text("awaiting info from phone")
+                } else{
+                    ScrollView{
+                        VStack{
+                            if connector.updating{
+                                let _ = self.update()
+                                Text("updating...")
+                            }
+                            NavigationLink(destination: DisplayInfo(currPattern: connector.pattern)){
+                                Text("View Patterns")
+                            }
+                            if timerObj.end{
+                                Button(action:{
+                                    print("starting....")
+                                    timerObj.toggleEnd()
+                                    timerObj.startOverallTimer()
+                                }){
+                                    Text("Start")
+                                }
+                            }
+                            else{
+                                Button(action:{
+                                    timerObj.toggleEnd()
+                                    print("stopping")
+                                }){
+                                    Text("stop")
+                                }
+                                ZStack{
+                                    IndicatorView(animationState: timerObj.colorState)
+                                    Text("\(timerObj.currentData, specifier: "%.2f")")
+                                }
+                                
+                            }
                         }
                     }
                     
-                    if connector.received && runProgram{
-                        let _ = self.updateTimer()
-                    }
                 }
             }
         }
-        .onAppear(perform: ExtendedSess.startExtendedSession)
-//        .onAppear(perform: startSession)
     }
 }
-
-#Preview {
-    WatchView( animationState: AnimationState.at)
-}
-
